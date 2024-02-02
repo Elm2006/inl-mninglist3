73375e7670ee58a0d5a5217a380c5fa172713e67
Knugen <isel06002@utb.vaxjo.se>
2024-02-02 12:57:04 +0100

    Add project files.

---
+++ inlämninglist3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+namespace inlämninglist3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string,int> storlek = new Dictionary<string,int>();
+            storlek.Add("Sverige", 450);
+            storlek.Add("Norge", 385);
+            storlek.Add("Danmark", 43);
+            storlek.Add("Finland", 338);
+            Console.WriteLine("Skriv in ett land i Norden");
+            string svar = Console.ReadLine();
+            Console.WriteLine(svar + " är " + storlek[svar] + " är hundratals kvadratkilometer stort");
+        }
+    }
+}
